e2f6425034c0b62507996daf4db54de4fd41fa47
hqv1 <v.hoang@outlook.com>
2017-07-12 03:46:16 -0700

    Added interface for structured logging. Based on serilog

---
+++ Common/Logging/ILoggerStructured.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Hqv.CSharp.Common.Logging
+{
+    /// <summary>
+    /// Interface for structured logging. See https://github.com/serilog.
+    /// </summary>
+    public interface ILoggerStructured
+    {
+        void LogDebug(string messageTemplate, params object[] propertyValues);
+        void LogDebug(Exception exception, string messageTemplate, params object[] propertyValues);
+
+        void LogInfo(string messageTemplate, params object[] propertyValues);
+        void LogInfo(Exception exception, string messageTemplate, params object[] propertyValues);
+
+        void LogWarning(string messageTemplate, params object[] propertyValues);
+        void LogWarning(Exception exception, string messageTemplate, params object[] propertyValues);
+
+        void LogError(string messageTemplate, params object[] propertyValues);
+        void LogError(Exception exception, string messageTemplate, params object[] propertyValues);
+
+        void LogFatal(string messageTemplate, params object[] propertyValues);
+        void LogFatal(Exception exception, string messageTemplate, params object[] propertyValues);
+    }
+}
