28758c11221ccdc70bfcc6a059972921ea3c9c83
hqv1 <v.hoang@outlook.com>
2017-07-14 04:59:37 -0700

    Added validation static class

---
+++ Common.Test/Validations/ValidationTest.cs
@@ -0,0 +1,77 @@
+﻿using FluentValidation;
+using Hqv.CSharp.Common.Exceptions;
+using Hqv.CSharp.Common.Validations;
+using Xunit;
+
+namespace Hqv.Csharp.Common.Test.Validations
+{
+    public class ValidationTest
+    {
+        private Widget _widget;
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void Should_Validate_WhenCalleeCreatesValidator()
+        {
+            GivenAValidWidget();
+            Validator.Validate<Widget, WidgetValidator>(_widget);
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void Should_ThrowValidationError_WhenCalleeCreatesValidator()
+        {
+            GivenAinvalidWidget();
+
+            var ex = Assert.Throws<HqvException>(() => Validator.Validate<Widget, WidgetValidator>(_widget));
+        }
+        
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void Should_Validate_WhenCallerCreatesValidator()
+        {
+            GivenAValidWidget();
+            Validator.Validate(_widget, new WidgetValidator());
+        }
+
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void Should_ThrowValidationError_WhenCallerCreatesValidator()
+        {
+            GivenAinvalidWidget();
+
+            var ex = Assert.Throws<HqvException>(() => Validator.Validate(_widget, new WidgetValidator()));
+        }
+
+        private void GivenAValidWidget()
+        {
+            _widget = new Widget("Blue", 10);
+        }
+
+        private void GivenAinvalidWidget()
+        {
+            _widget = new Widget("Blue", 130);
+        }
+    }
+
+    public class Widget
+    {
+        public Widget(string color, int width)
+        {
+            Color = color;
+            Width = width;
+        }
+        public string Color { get; set; }
+        public int Width { get; set; }
+    }
+
+    public class WidgetValidator : AbstractValidator<Widget>
+    {
+        public WidgetValidator()
+        {
+            RuleFor(x => x.Color).NotNull().NotEmpty();
+            RuleFor(x => x.Width).GreaterThan(0).LessThanOrEqualTo(100);
+        }
+    }
+}
---
+++ Common/Validations/Validator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using Hqv.CSharp.Common.Exceptions;
+
+namespace Hqv.CSharp.Common.Validations
+{
+    public static class Validator
+    {
+        public static void Validate<TT, TValidator>(TT obj)
+            where TValidator : AbstractValidator<TT>, new()
+        {
+            var validator = new TValidator();
+            var validationResult = validator.Validate(obj);
+            if (validationResult.IsValid) return;
+
+            var exception = new HqvException("Validation failed");
+            exception.Data["errors"] = validationResult.Errors;
+            throw exception;
+        }
+
+        public static void Validate<TT, TValidator>(TT obj, TValidator validator)
+            where TValidator : AbstractValidator<TT>, new()
+        {            
+            var validationResult = validator.Validate(obj);
+            if (validationResult.IsValid) return;
+
+            var exception = new HqvException("Validation failed");
+            exception.Data["errors"] = validationResult.Errors;
+            throw exception;
+        }
+    }
+}
