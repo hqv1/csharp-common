c1caf09bf291773bcd76aeccf77b68ba0ccd144a
hqv1 <v.hoang@outlook.com>
2017-09-25 21:27:55 -0700

    Added IAggregateRoot.

---
+++ Common/Entities/IAggregateRoot.cs
@@ -0,0 +1,10 @@
+﻿namespace Hqv.CSharp.Common.Entities
+{
+    /// <summary>
+    /// Domain Driven Design (DDD) concept. The root entity. This is a marker interface.
+    /// </summary>
+    public interface IAggregateRoot
+    {
+        
+    }
+}
